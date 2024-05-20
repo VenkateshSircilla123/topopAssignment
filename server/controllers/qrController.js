721e4a514cc02daeac22ba67db59705fa1193b8d
VenkateshSircilla123 <sircillavenkatesh0@gmail.com>
2024-05-20 22:51:27 +0530

    getqr added

--- server/controllers/qrController.js
+++ server/controllers/qrController.js
@@ -17,3 +17,16 @@ export const createqr = async (req, res) => {
     console.log(error);
   }
 };
+
+export const getqr = async (req, res) => {
+  try {
+    const qr = await QrCode.findById(req.params.id);
+    if (!qr) {
+      res.status(404).send("Qr not found");
+    }
+    res.status(200).send(qr);
+  } catch (error) {
+    res.status(400).send(error);
+    console.log(error);
+  }
+};
--- server/routes/rqRoute.js
+++ server/routes/rqRoute.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { createqr } from "../controllers/qrController.js";
+import { createqr, getqr } from "../controllers/qrController.js";
 
 const router = Router();
 router.post("/gen", createqr);
+router.get("/user/:id", getqr);
 
 export default router;
